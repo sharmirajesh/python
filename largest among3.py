a605c84cf4bac91931b63beb10881cc4a0d79944
sharmirajesh <48148551+sharmirajesh@users.noreply.github.com>
2019-08-05 12:17:53 +0530

    Create largest among3.py

---
+++ largest among3.py
@@ -0,0 +1,9 @@
+a=int(input("enter a value:"))
+b=int(input("enter a value:"))
+c=int(input("enter a value:"))
+if(a>b)and(a>c):
+   print("a is grater")
+elif(b>a)and(b>c):
+    print("b is greater")
+else:
+    print("c is greater")
